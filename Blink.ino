4e527c6e1016e8b4e1336e0f0433a939cef3ae1a
Foad <155805725+FoadTeymoori@users.noreply.github.com>
2026-06-14 03:05:48 -0700

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,35 @@
+/////////////////////////////////////1
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                     
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);                      
+}
+//////////////////////////////////////2
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);  
+  delay(1000);                     
+  digitalWrite(13, LOW);   
+  delay(1000);                      
+}
+//////////////////////////////////////3 
+int LED = 2;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);  
+  delay(1000);                     
+  digitalWrite(LED, LOW);   
+  delay(1000);                      
+}
+
